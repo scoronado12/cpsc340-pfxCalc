eda4e2b057ef1f700b736462545bf398fa19dc41
Stefano C. Coronado <stefanocorona12@gmail.com>
2019-04-07 13:16:41 -0400

    TODO plop stuff onto stack

--- main.cpp
+++ main.cpp
@@ -4,20 +4,23 @@
 
 using namespace std;
 
+int stackProcessor(string pfxExpression);
+
+
 int main(){
     
     string pfxIn;
     int resultant;
     
     while (true){
-        cout << "Enter a postfix expression or quit to exit:" << endl;
+        cout << "Enter a postfix expression or quit to exit:";
         getline(cin,pfxIn);
         
         if (pfxIn == "exit" || pfxIn == "quit"){
             exit(0);
-        }else{
-            resultant = stackProcessor(pfxIn);
-            cout << resultant;
+        }else {
+            stackProcessor(pfxIn);
+//             cout << resultant;
         }
         
     }
@@ -27,5 +30,11 @@ int main(){
 
 int stackProcessor(string pfxExpression){
     
+    stack <char> myStack;
     
+    for (char c: pfxExpression){
+        cout << c << endl;
+    }
+    
+    return 0;
 }
