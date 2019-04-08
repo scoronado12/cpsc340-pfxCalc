a105039c867cc9266244b6e262c3e9336aa1279b
Stefano C. Coronado <stefanocorona12@gmail.com>
2019-04-08 19:26:13 -0400

    added planned function declarations

--- main.cpp
+++ main.cpp
@@ -1,40 +1,42 @@
 #include <iostream>
 #include <stack>
 #include <string>
+#include <cctype> //for isdigit()
+#include <cmath> //for pow() func
+#include <cstdlib> //atof(char num)
 
 using namespace std;
 
-int stackProcessor(string pfxExpression);
+void stringParser(string pfxExpression, stack <double> myStack); //parseLine
+void addToStack(double);
+double operate();
 
 
 int main(){
-    
     string pfxIn;
     int resultant;
-    
+
+    stack <double> myStack;
+
     while (true){
         cout << "Enter a postfix expression or quit to exit:";
         getline(cin,pfxIn);
-        
         if (pfxIn == "exit" || pfxIn == "quit"){
             exit(0);
         }else {
-            stackProcessor(pfxIn);
-//             cout << resultant;
+            stringParser(pfxIn, myStack);
         }
-        
     }
 
     return 0;
 }
 
-int stackProcessor(string pfxExpression){
-    
-    stack <char> myStack;
-    
+
+void stringParser(string pfxExpression, stack <double> myStack){
+
+
     for (char c: pfxExpression){
         cout << c << endl;
     }
-    
-    return 0;
+
 }
