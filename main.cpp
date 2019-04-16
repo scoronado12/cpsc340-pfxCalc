f85592e6c2fc92cb4de7c82f9a20e77b9a3fdf87
Stefano C. Coronado <stefanocorona12@gmail.com>
2019-04-16 10:03:56 -0400

    Revert "fixed issues with bloated algorithim, need to catch stuff like 1 2 3 4 * as being invalid"
    This reverts commit b612fb9666f3914c4586267772c436dffce9091f.

--- main.cpp
+++ main.cpp
@@ -14,9 +14,9 @@
 
 using namespace std;
 
-void stringProcessor(string pfxExpression, stack <double> &mystl, vector <string> &operadores);
+void stringProcessor(string pfxExpression, stack <double> &mystl, stack <string> &operadores);
 float operate(double operand1, double operand2, string operador);
-void cleanStacks(stack <double> &mystl);
+void cleanStacks(stack <double> &mystl, stack <string> &operadores);
 bool isOperator(string spot);
 
 
@@ -30,7 +30,7 @@ int main(){
     string pfxIn;
     int resultant;
     stack <double> mystl;
-    vector <string> operadores;
+    stack <string> operadores;
 
     while (true){
         cout << "Enter a postfix expression or quit to exit:";
@@ -49,10 +49,11 @@ int main(){
  * stringProcessor
  *  @param stack <double> mystl - stack of numbers  - passed by reference
  *  @param stack <string> operadores - stack of operators - passed by reference
+ * 
  */
 
 
-void stringProcessor(string pfxExpression, stack <double> &mystl, vector <string> &operadores){
+void stringProcessor(string pfxExpression, stack <double> &mystl, stack <string> &operadores){
     stringstream expressionIn(pfxExpression);
     vector<string> contents;
     string thing = "";
@@ -64,46 +65,58 @@ void stringProcessor(string pfxExpression, stack <double> &mystl, vector <string
     }
     //format check - used some 326 logic here
     for (int i = 0 ; i < contents.size(); i++){
-         //cout << "checking " << contents.at(i) << endl;
+//         cout << "checking " << contents.at(i) << endl;
         if (!(isOperator(contents.at(i)) || contents.at(i) == " " || isdigit(contents.at(i).at(0)))){
             cout << "Invalid Expression!" << endl;
-              //cout << "First check failed" << endl;
+//              cout << "First check failed" << endl;
             main();
         }
     }
     //stack adding
     for (int i = 0; i < contents.size(); i++) {
         if (isOperator(contents.at(i))){
-            operadores.push_back(contents.at(i));
+            operadores.push(contents.at(i));
         } else if (isdigit(contents.at(i).at(0))){ // I know this looks dirty but isdigit doesn't like the entire number in there as an int/double/etc
             mystl.push(stod(contents.at(i)));
         }
     }
 
     /*if mystl < operator */
-    if (mystl.size() < operadores.size()){ // 2nd format check check if expression is invalid
+    
+    if ((mystl.size() < operadores.size() || (/*TODO 1 2 3 4 * must be caught */) )){ // 2nd format check check if expression is invalid
         cout << "Invalid Expression!" << endl;
-        cleanStacks(mystl); // clean up just in case
+        cleanStacks(mystl, operadores); // clean up just in case
         main();
-         //cout << "second check failed" << endl;
-         //cout << "mystl " << mystl.size() << endl;
-         //cout << "operators " << operadores.size() << endl;
+//         cout << "second check failed" << endl;
+//         cout << "mystl " << mystl.size() << endl;
+//         cout << "operators " << operadores.size() << endl;
     }
     //time to empty all of them doing an operation
-     while (mystl.size() > 1){
-         
-         double op1 = mystl.top();
+     double operand2 = 0;
+     while (!(mystl.empty())){
+
+         double operand1 = mystl.top();
          mystl.pop();
-         double op2 = mystl.top();
-         mystl.pop();
-         
-         solution = operate(op1, op2, operadores.at(0));
-         operadores.pop_back();
-         mystl.push(solution);
-         
-         
+         if (mystl.empty()){
+            //TODO check if this needs to be different
+            solution = operate(operand1, solution, operadores.top());
+            operadores.pop();
+         }else {
+             operand2 = mystl.top();
+             mystl.pop();
+         }
+         if (!operadores.empty()){
+            string operador = operadores.top();
+            operadores.pop();
+            solution = solution + operate(operand1, operand2, operador);
+         } /*else {
+             cout << "Invalid Expression!" << endl;
+             cout << "Third check" << endl;
+             cleanStacks(mystl, operadores);
+             main();
+         }*/
      }
-     cleanStacks(mystl); //empty them just in case.
+     cleanStacks(mystl, operadores); //empty them just in case.
      expressionIn.clear(); //clear stringstream
      cout << solution << endl;
 }
@@ -114,11 +127,14 @@ void stringProcessor(string pfxExpression, stack <double> &mystl, vector <string
  * @param operadores <string> - stack of operators
  */
 
-void cleanStacks(stack <double> &mystl){
+void cleanStacks(stack <double> &mystl, stack <string> &operadores){
     while (!mystl.empty()){
         mystl.pop();
     }
-
+    
+    while (!operadores.empty()){
+        operadores.pop();
+    }
     
 }
 
